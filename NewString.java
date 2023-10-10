a61a8f5d0e1632c79c37276cbdfb76721cfba9e2
Alessandro <sandroscro@libero.it>
2023-10-10 14:43:52 +0200

    Added NewString

---
+++ NewString.java
@@ -0,0 +1,8 @@
+public class NewString {
+    public static void main(String args[]) {
+        char a = 'a';
+        char b = 'b';
+        System.out.println("The new string is: " + a + b);
+    }
+    
+}
